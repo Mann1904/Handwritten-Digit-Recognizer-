66390c0d7f9cf97c50498a61cb0e3b16abc26519
Mann Saradva <58176237+Mann1904@users.noreply.github.com>
2020-10-30 08:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def parseImage(imgData):
 
 if __name__ == '__main__':
     app.debug = True
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+ 
